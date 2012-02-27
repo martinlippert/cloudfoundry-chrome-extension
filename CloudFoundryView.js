23f63a2228bead49341db87d916bb5cddcf9a371
Martin Lippert <lippert@acm.org>
2012-02-28 00:58:57 +0100

    extracted JavaScript code into separate file and made link to open another window

---
+++ CloudFoundryView.js
@@ -0,0 +1,63 @@
+function check(form) {
+	chrome.extension.sendRequest({ action: "login", cloudurl: form.cloudurl.value, userid: form.userid.value, userpasswd: form.pswrd.value },
+    	loginCallback);
+}
+
+loginCallback = function(loginResult) {
+	alert(loginResult.message);
+	chrome.extension.sendRequest({ action: "getApplications"}, getAppsCallback);
+}
+
+getAppsCallback = function(appsResult) {
+	if (appsResult.message) {
+		alert("Error in getting deployed apps: " + appsResult.message);			
+	}
+	else {
+		for (var i = 0; i < appsResult.length; i++) {
+			chrome.extension.sendRequest({ action: "getInstances", appName: appsResult[i].name}, getInstancesCallback);
+		}
+	}
+}
+
+getInstancesCallback = function(instancesResult) {
+	if (instancesResult.message) {
+		alert("Error in getting deployed apps: " + instancesResult.message);			
+	}
+	else {
+		for (var i = 0; i < instancesResult.instances.length; i++) {
+			if (instancesResult.instances[i].debug_port != null) {
+				insertLink(instancesResult.name, instancesResult.instances[i].index, instancesResult.instances[i].debug_port);
+			}
+			else {
+				insertText(instancesResult.name, instancesResult.instances[i].index);
+			}
+		}
+	}
+}
+
+insertLink = function(appName, instanceName, debugPort) {
+	var div = document.getElementById('instances');
+	var a = document.createElement('a');
+	a.innerText = 'Application: ' + appName + ' - Instance: ' + instanceName;
+	
+	var url = 'http://node-inspector.vcap.me/debug?port=' + debugPort;
+	
+	a.href = 'javascript:openInspectorWindow(\'' + url + '\');';
+	div.appendChild(a);
+	
+	var p = document.createElement('p');
+	div.appendChild(p);
+}
+
+insertText = function(appName, instanceName) {
+	var div = document.getElementById('instances');
+	var a = document.createTextNode('Application: ' + appName + ' - Instance: ' + instanceName);
+	div.appendChild(a);
+	
+	var p = document.createElement('p');
+	div.appendChild(p);
+}
+
+openInspectorWindow = function(url) {
+	window.open(url, undefined, 'location=0');
+}
